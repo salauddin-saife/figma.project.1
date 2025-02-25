a35f1dd0ffd2e497f092584895deff94726be19a
salauddin-saife <salauddin15saife@gmail.com>
2025-02-25 19:55:15 +0600

    ok

---
+++ src/Components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = ({Button_text}) => {
+  return (
+    <div>
+        <div className='w-[142px] h-[51px] bg-red-500 text-center flex justify-center items-center bottom-[1px] border-[#FFFFFF]'>
+            <h1>Button</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Button
