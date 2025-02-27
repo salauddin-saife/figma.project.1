23c3649ab9208c48e2212897b5fb89df376e5e0f
salauddin-saife <salauddin15saife@gmail.com>
2025-02-27 19:49:30 +0600

    up

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Join from '../Components/Join'
 import Footer from '../Components/Footer'
+import Cart from '../Components/Cart'
+import Cart_part from '../Components/Cart_part'
 
 const Services = () => {
   return (
     <div>
         <Navbar/>
+        <Cart_part/>
         <Join/>
         <Footer/>
     </div>
