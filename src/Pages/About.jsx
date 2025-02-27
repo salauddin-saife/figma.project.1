32d8f0d873b67c6c6c5c5d01c04da85b3c2f841e
salauddin-saife <salauddin15saife@gmail.com>
2025-02-27 19:47:30 +0600

    up

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Join from '../Components/Join'
 import Footer from '../Components/Footer'
+import Learn_more from '../Components/Learn_more'
+import Four_section from '../Components/Four_section'
+import Slider from '../Components/Slider'
 
 const About = () => {
   return (
     <div>
         <Navbar/>
+        <Learn_more/>
+        <Slider/>
         <Join/>
         <Footer/>
     </div>
